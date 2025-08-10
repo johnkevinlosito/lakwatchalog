ffb848d9116ee0232131abd1e330fcc2451cb01b
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2025-08-10 19:31:23 +0800

    setup prisma + better-auth

---
+++ src/env/server.ts
@@ -0,0 +1,11 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.url(),
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.string().min(1),
+  },
+  experimental__runtimeEnv: process.env,
+});
