59c9f2ef7cbed52d256fa67fb50b6d329c26f761
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2025-08-14 17:54:25 +0800

    added signout to sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import * as React from "react";
-import { CirclePlus, Map } from "lucide-react";
+import { CirclePlus, LogOut, Map } from "lucide-react";
 
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import SidebarItems from "./sidebar-items";
+import { signOut } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 const data = {
   navMain: [
@@ -26,6 +31,16 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const router = useRouter();
+  const handleLogOut = async () => {
+    await signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+      },
+    });
+  };
   return (
     <Sidebar
       className="top-(--header-height) h-[calc(100svh-var(--header-height))]!"
@@ -35,7 +50,15 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarContent>
         <SidebarItems items={data.navMain} />
       </SidebarContent>
-      <SidebarFooter></SidebarFooter>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton onClick={handleLogOut}>
+              <LogOut /> Log out
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 }
